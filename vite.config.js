9f22c003a51e9aeea4df5fcd0fe2708924ba6662
srbutler1 <srb019@uark.edu>
2025-01-15 10:50:34 -0600

    vite config bug fix

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import { sitemap } from 'vite-plugin-sitemap'
-
-const domain = 'https://arkansasaifoundry.com' 
+import ViteSitemap from 'vite-plugin-sitemap'
 
 export default defineConfig({
-  base: '/', // Base is just / for custom domains
+  base: '/',
   plugins: [
     react(),
-    sitemap({
-      hostname: domain,
-      routes: ['/'], // Just the home page for now
+    ViteSitemap({
+      hostname: 'https://arkansasaifoundry.com',
+      routes: ['/'],
       lastmod: new Date().toISOString(),
       changefreq: 'weekly',
       priority: 1.0
