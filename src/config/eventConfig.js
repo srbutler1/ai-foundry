b87787218d19993d0e945c58b3152ee2db5fb4a2
srbutler1 <srb019@uark.edu>
2025-03-02 13:48:30 -0600

    new event config

--- src/config/eventConfig.js
+++ src/config/eventConfig.js
@@ -7,7 +7,7 @@ export const eventConfig = {
     time: "6:00 PM – 7:00 PM",
     location: "Willard J. Walker Hall (WJWH) | University of Arkansas Room 203 191 N Harmon Ave, Fayetteville, AR 72701",
     description: "Join us for an insightful session with Jarrod Ramsey from Microsoft as he discusses how leaders in tech are actively reshaping the AI landscape.",
-    registrationLink: "https://docs.google.com/forms/d/e/1FAIpQLSekIZCNIuUeLjcsTo_zOJBj3Yd0a8ciuLTX_akmv1rlxD3tkg/viewform?usp=dialog",
+    registrationLink: "https://docs.google.com/forms/d/e/1FAIpQLSc1s9DBO06tBvN6lp5Xt18HpQOzW_AKiwctqXzVuEMoRn0yTg/viewform?usp=dialog",
     groupMeLink: "https://groupme.com/join_group/105347053/9oA8jhjn",
     flyerImage: "/images/Microsoft_Event.png"
   }
